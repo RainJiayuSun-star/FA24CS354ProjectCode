ed412396e6872af02538e90cde03657e29f35c2c
RainJiayuSun-star <145049305+RainJiayuSun-star@users.noreply.github.com>
2024-09-25 22:35:33 -0500

    project finished

--- p2a/check_sudoku_board.c
+++ p2a/check_sudoku_board.c
@@ -1,30 +1,6 @@
-////////////////////////////////////////////////////////////////////////////////
-// Main File:        check_sudoku_board.c
-// This File:        check_sudoku_board.c
-// Other Files:      board1.txt
-// Semester:         CS 354 Lecture 003? Fall 2023
-// Grade Group:      gg17
-// Instructor:       deppeler
-// 
-// Author:           Rain (Jiayu) Sun
-// Email:            jsun424@wisc.edu
-// CS Login:         rains
-//
-///////////////////////////  OPTIONAL WORK LOG  //////////////////////////////
-//  Document your work sessions here or on your copy http://tiny.cc/work-log
-//  Keep track of commands, structures, code that you have learned.
-//  This will help you focus your review on this from each program that
-//  are new to you. There is no need to submit work log.
-/////////////////////////// OTHER SOURCES OF HELP ////////////////////////////// 
-// Persons:          Identify persons by name, relationship to you, and email.
-//                   Describe in detail the the ideas and help they provided.
-//
-// Online sources:   avoid web searches to solve your problems, but if you do
-//                   search, be sure to include Web URLs and description of 
-//                   of any information you find.
-// 
-// AI chats:         save a transcript and submit with project.
-//////////////////////////// 80 columns wide ///////////////////////////////////
+// Add your file header here
+// Fix compiler error
+// Implement remaining functionality
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -37,7 +13,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // For malloc and free
 #include <string.h>
 
 char *DELIM = ",";  // commas ',' are a common delimiter character for data strings
@@ -57,36 +33,9 @@ char *DELIM = ",";  // commas ',' are a common delimiter character for data stri
  * size:  number of rows and columns in the board
  */
 int valid_sudoku_board(int **board, int size) {
-	// check if there are any duplicates in the rows
-	for (int i = 0; i < size; i++) {
-		int row_check[size+1];
-		memset(row_check, 0, sizeof(row_check));
-		for (int j = 0; j < size; j++) {
-			int val = board[i][j];
-			if (val != 0) {
-				if (row_check[val] == 1) {
-					return 0;
-				}
-				row_check[val] = 1;
-			} 
-		}
-	}
-	// check if there are any duplicates in the columns 
-	for (int i = 0; i < size; i++) {
-		int column_check[size+1];
-		memset(column_check, 0, sizeof(column_check));
-		for (int j = 0; j < size; j++) {
-			int val = board[j][i];
-			if (val != 0) {
-				if (column_check[val] == 1) {
-					return 0; // Duplicate found
-				}
-				column_check[val] = 1;
-			}
-		}
-	}
 
-	return 1;   
+
+	return 0;   
 }    
 
 /* COMPLETED (DO NOT EDIT)       
@@ -134,11 +83,7 @@ void get_board_size(FILE *fptr, int *size) {
  */
 int main( int argc, char **argv ) {              
 
-	// DONE: Check if number of command-line arguments is correct.
-	if (argc != 2) {
-		printf("Please enter the correct command-line argument. Usage: %s <filename>\n", argv[0]);
-		exit(1);
-	}
+	// TODO: Check if number of command-line arguments is correct.
 
 	// Open the file 
 	FILE *fp = fopen(*(argv + 1), "r");
@@ -150,25 +95,10 @@ int main( int argc, char **argv ) {
 	// will store the board's size, number of rows and columns
 	int size;
 
-	// DONE: Call get_board_size to read first line of file as the board size.
-	get_board_size(fp, &size);
+	// TODO: Call get_board_size to read first line of file as the board size.
 
-	// DONE: Dynamically allocate a 2D array for given board size.
+	// TODO: Dynamically allocate a 2D array for given board size.
 	// You must dyamically create a array of pointers to other arrays of ints
-	int **board = (int **)malloc(size * sizeof(int *));
-	if (board == NULL) {
-		printf("Memory allocation failed.\n");
-		fclose(fp);
-		exit(1);
-	}
-	for (int i = 0; i < size; i++) {
-		board[i] = (int *)malloc(size * sizeof(int));
-		if (board[i] == NULL) {
-			printf("Memory allocation failed.\n");
-			fclose(fp);
-			exit(1);
-		}
-	}
 
 	// Read the remaining lines of the board data file.
 	// Tokenize each line and store the values in your 2D array.
@@ -185,26 +115,17 @@ int main( int argc, char **argv ) {
 
 		token = strtok(line, DELIM);
 		for (int j = 0; j < size; j++) {
-			// DONE: Complete the line of code below
+			// TODO: Complete the line of code below
 			// to initialize elements of your 2D array.
-			/* ADD ARRAY ACCESS CODE HERE */ board[i][j]= atoi(token); 
+			/* ADD ARRAY ACCESS CODE HERE */ = atoi(token); 
 			token = strtok(NULL, DELIM);
 		}
 	}
 
-	// DONE: Call valid_sudoku_board and print the appropriate
+	// TODO: Call valid_sudoku_board and print the appropriate
 	//       output depending on the function's return value.
-	if (valid_sudoku_board(board, size) == 1) {
-		printf("valid\n");
-	} else {
-		printf("invalid\n");
-	}
 
 	// TODO: Free dynamically allocated memory.
-	for (int i = 0; i < size; i++) {
-		free(board[i]);
-	}
-	free(board);
 
 	//Close the file.
 	if (fclose(fp) != 0) {
